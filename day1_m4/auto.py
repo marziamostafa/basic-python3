c0c4bf0842b097bff15602f6372e6be651ef8734
marziamostafa <marziamostafa@gmail.com>
2024-12-15 17:21:24 +0600

    explore PyAutoGUI

---
+++ day1_m4/auto.py
@@ -0,0 +1,14 @@
+import pyautogui
+import time
+
+
+# pyautogui.alert('This is an alert box. Exploring alert with pyautogui. ')
+# time.sleep(5)
+# pyautogui.write('Marzia learning python! ', interval=0.25)
+# pyautogui.press('enter')
+
+
+for i in range(1,5):
+    time.sleep(3)
+    pyautogui.write('Marzia learning python! ', interval=0.25)
+    pyautogui.press('enter')
