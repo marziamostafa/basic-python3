3b61b5df8d5d315947ff6a7ef6133502bbce6906
marziamostafa <marziamostafa@gmail.com>
2024-12-27 23:59:31 +0600

    list and slice

--- day1_m4/try.py
+++ day1_m4/try.py
@@ -5,3 +5,9 @@ except:  # we used 'catch' in java, c++, c#, javaScript
   print('something went wrong')
 finally:
   print('math is done')
+
+# else- when no exception occurs in the try block
+
+def display(**kwargs):
+    for i in kwargs:
+        print(i, end='')
---
+++ day2_m5/list.py
@@ -0,0 +1,22 @@
+num= [4,6,17,25,46,92,38]
+
+# indexing
+print(num[3])
+print(num[-2])
+
+# slice : take part of the array
+# need to mention starting index and ending index
+print(num[1:5])  # starting at 1 and ending at 4 (before 5)
+print(num[3:])    # from index 3 rest of everything (no ending index)
+print(num[3:-3])  # both way indexing
+
+# list [start : end : step]
+print(num[2:6:1])  # step will be incremented by 1
+print(num[2:6:2])  # step will be incremented by 2
+print(num[6:2:-1])  # step will be decremented by 1
+print(num[-2:2:-1])  # step will be incremented by 1
+print(num[-2:-8:-1])  # step will be incremented by 1
+print(num[-2:-8:-2])  # step will be incremented by 2
+
+print(num[:])  # no start and end; so full array will be printed
+print(num[: : -1]) # full array in reversed way
